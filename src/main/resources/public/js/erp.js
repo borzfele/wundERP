fac5a4bad8f9ac55cf5cb42558ded463fc392159
borzfele <borzfele@gmail.com>
2018-04-12 09:57:12 +0200

    javascriptin' finished, forms css-ed

--- src/main/resources/public/js/erp.js
+++ src/main/resources/public/js/erp.js
@@ -13,6 +13,7 @@ function populateSelectWithOptions($select, data) {
 function main() {
     $("#open-button").on("click", function () {
         $(this).hide();
+        $(".button-cont").hide();
         $("#open-form-cont").show();
     });
 
@@ -20,6 +21,7 @@ function main() {
         $(this).hide();
         $("#stats").hide();
         $("#pos-button-cont").hide();
+        $(".button-cont").hide();
         $("#close-form-cont").show();
     });
 
@@ -27,11 +29,13 @@ function main() {
         $("#close-button").show();
         $("#stats").show();
         $("#pos-button-cont").show();
+        $(".button-cont").show();
         $("#close-form-cont").hide();
     });
 
     $("#cancel-open").on("click", function () {
         $("#open-form-cont").hide();
+        $(".button-cont").show();
         $("#open-button").show();
     });
 
@@ -46,6 +50,7 @@ function main() {
     $("#add-income").on("click", function () {
         $("#issue").prop('disabled', true);
         $(".button-cont").hide();
+        $("#open-button").attr("disabled", true);
         $("#income-form-cont").show();
         $.get("/get-issues", function(response, status){
             populateSelectWithOptions($("#issue"), response.transactionIssues);
@@ -54,11 +59,13 @@ function main() {
 
     $("#cancel-income").on("click", function () {
         $("#income-form-cont").hide();
+        $("#open-button").attr("disabled", false);
         $(".button-cont").show();
     });
 
     $("#add-expense").on("click", function () {
         $("#expense-issue").prop('disabled', true);
+        $("#open-button").attr("disabled", true);
         $(".button-cont").hide();
         $("#expense-form-cont").show();
         $.get("/get-issues", function(response, status){
@@ -68,14 +75,15 @@ function main() {
 
     $("#cancel-expense").on("click", function () {
         $("#expense-form-cont").hide();
+        $("#open-button").attr("disabled", false);
         $(".button-cont").show();
     });
 
     $("#close-form").on("submit", function (e) {
-        let closeCash = $("#closeCash").val();
-        let terminalBalance = $("#terminalBalance").val();
-        let cassaBalance = $("#cassaBalance").val();
-        let posBalance = $("#posBalance").val();
+        let closeCash = $("#close-cash").val();
+        let terminalBalance = $("#terminal-balance").val();
+        let cassaBalance = $("#cassa-balance").val();
+        let posBalance = $("#pos-balance").val();
         if (!isFinite(closeCash) || closeCash === "" ||
             !isFinite(terminalBalance) || terminalBalance === "" ||
             !isFinite(cassaBalance) || cassaBalance === "" ||
